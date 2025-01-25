f2ed4d2c56b31e4c2961a6341ef3ca0c9c53b868
Hariharan Sivakumar <hariharansivakumar@affordmed.com>
2025-01-24 20:30:05 -0500

    print dns

--- extract_dns.sh
+++ extract_dns.sh
@@ -2,3 +2,4 @@ terraform output -raw aws_ec2_instance_public_dns > dns_output.txt
 cat dns_output.txt
 DNS=$(cat dns_output.txt | awk -F'::' '{print $1}' | tr -d '\n')
 echo "public_dns=$DNS" >> $GITHUB_OUTPUT
+echo "dns is $DNS"
